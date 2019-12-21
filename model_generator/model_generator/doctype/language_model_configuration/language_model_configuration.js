4e3490bdcc3eca6205ce9d39e0d756afe59f66af
assemmarwan <assem905@gmail.com>
2019-12-21 12:54:06 +0400

    Init Language Model Configurations

---
+++ model_generator/model_generator/doctype/language_model_configuration/language_model_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Assem Marwan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Language Model Configuration', {
+	// refresh: function(frm) {
+
+	// }
+});
